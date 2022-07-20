b788efedbcf66d635480a5cc812e2c2178a958b3
saurabhfegade <saurabh.fegade1998@gmail.com>
2022-07-20 10:00:42 +0530

    setup tailwind and atomic structure directory

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div>
+        <h2>Frontend Starter Repo</h2>
+      </div>
+    </>
+  );
+};
+
+export default Home;
